3de396b7f7454321c173663abe23adaddf3621cf
Tomasz Paterka <tomekpaterka@gmail.com>
2018-07-08 20:54:13 +0200

    add components

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+	render() {
+		return (
+			<div>
+				<h2>Got questions?</h2>
+				<p>contact us + 48 0000 66484 00 </p>
+			</div>
+		);
+	}
+}
+
+export default Contact;
